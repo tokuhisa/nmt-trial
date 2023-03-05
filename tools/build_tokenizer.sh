719979821c9d5e600573dc7d16a37fd57d38a8a0
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2023-03-05 16:15:19 +0900

    :+1: Fix build-tokenzier.yml

---
+++ tools/build_tokenizer.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+set -x
+
+ROOT_DIR=$PWD
+ICU_VERSION=${ICU_VERSION:-72.1}
+
+mkdir bin
+
+curl -L -O -nv https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-Win64-MSVC2019.zip
+unzip *.zip
+unzip *_Release/icu-windows.zip -d icu
+rm -r *_Release
+
+rm -rf build
+mkdir build
+cd build
+cmake -DLIB_ONLY=ON -DICU_ROOT=$ROOT_DIR/icu/ -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/bin/ ..
+cmake --build . --config Release --target install
+
+cp $ROOT_DIR/icu/bin64/icudt*.dll $ROOT_DIR/bin/
+cp $ROOT_DIR/icu/bin64/icuuc*.dll $ROOT_DIR/bin/
