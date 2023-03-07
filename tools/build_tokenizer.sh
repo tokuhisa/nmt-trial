461f759d7dbf3824409c1cac8e0d548327a93aee
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2023-03-07 21:22:00 +0900

    :+1: Fix build_tokenizer.sh

--- tools/build_tokenizer.sh
+++ tools/build_tokenizer.sh
@@ -7,19 +7,23 @@ ROOT_DIR=$PWD
 ICU_VERSION=${ICU_VERSION:-72.1}
 
 mkdir outputs
+mkdir outputs/icu
+mkdir outputs/Release
+mkdir outputs/Debug
 
 curl -L -O -nv https://github.com/unicode-org/icu/releases/download/release-${ICU_VERSION/./-}/icu4c-${ICU_VERSION/./_}-Win64-MSVC2019.zip
 unzip *.zip
 unzip *_Release/icu-windows.zip -d icu
 rm -r *_Release
 
+cp $ROOT_DIR/icu/bin64/icudt*.dll $ROOT_DIR/outputs/icu/
+cp $ROOT_DIR/icu/bin64/icuuc*.dll $ROOT_DIR/outputs/icu/
+
 rm -rf build
 mkdir build
 cd build
-cmake -DLIB_ONLY=OFF -DICU_ROOT=$ROOT_DIR/icu/ -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/outputs/ -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --config Debug --target install
+cmake -DLIB_ONLY=OFF -DICU_ROOT=$ROOT_DIR/icu/ -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/outputs/Release/ -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release --target install
 
-cp $ROOT_DIR/icu/bin64/icudt*.dll $ROOT_DIR/outputs/bin/
-cp $ROOT_DIR/icu/bin64/icuuc*.dll $ROOT_DIR/outputs/bin/
-cp $ROOT_DIR/icu/lib64/icudt*.lib $ROOT_DIR/outputs/lib/
-cp $ROOT_DIR/icu/lib64/icuuc*.lib $ROOT_DIR/outputs/lib/
+cmake -DLIB_ONLY=OFF -DICU_ROOT=$ROOT_DIR/icu/ -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/outputs/Debug/ -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . --config Debug --target install
