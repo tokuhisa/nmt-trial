63ea554b7ce5e4ec12980e9a0ce4c54cee09fde1
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2023-03-09 01:32:43 +0900

    :+1: Fix translate debug build

--- tools/build_translate.sh
+++ tools/build_translate.sh
@@ -34,5 +34,6 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/outputs/Release -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/windows/compiler/lib/intel64_win;C:/oneDNN/Release" -DBUILD_CLI=OFF -DWITH_DNNL=ON ..
 cmake --build . --config Release --target install --parallel 2 --verbose
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/outputs/Debug -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/windows/compiler/lib/intel64_win;C:/oneDNN/Release" -DBUILD_CLI=OFF -DWITH_DNNL=ON ..
+# cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/outputs/Debug -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/Intel/oneAPI/compiler/latest/windows/compiler/lib/intel64_win;C:/oneDNN/Debug" -DBUILD_CLI=OFF -DWITH_DNNL=ON ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/outputs/Debug -DBUILD_CLI=OFF -DWITH_DNNL=OFF ..
 cmake --build . --config Debug --target install --parallel 2 --verbose
